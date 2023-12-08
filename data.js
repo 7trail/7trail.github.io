5204233ef72b456c9211e673e110da72aca54a6c
7trail <72806227+7trail@users.noreply.github.com>
2023-12-07 18:49:12 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,13 @@
 const url = "https://stimulatingcomplexdirectories-json-store-1--coder100.repl.co/db/a961b31c-6e2f-498a-93bc-8038431205ee";
 	async function getDb(offset="") {
-	    return await fetch(url+offset, {
+		var value = {};
+	     await fetch(url+offset, {
 	      method: "GET"
-	    }).then(r =>  r.json());
+	    }).then(r =>  {
+		value = r.json();
+		    
+	    });
+		return value;
 	  }
 
 	function getData(id) {
